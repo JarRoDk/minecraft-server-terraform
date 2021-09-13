e3218038a5aaa85082987ba26adebcfa9e1d9494
JarRoD <jaroslaw.nyka@tameshi.pl>
2021-09-13 02:03:12 +0200

    FEAT: add EULA true add tmux and zsh

--- main.tf
+++ main.tf
@@ -1,18 +1,5 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "3.83.0"
-    }
-  }
-}
-
-provider "google" {
-  # Configuration options
-}
-
 resource "google_compute_instance" "default" {
-  name         = "test"
+  name         = "minecraft-temporary-server-centos7"
   machine_type = "e2-medium"
   zone         = "europe-central2-a"
   project = "quantum-bonus-325717"
@@ -20,9 +7,12 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "centos-cloud/centos-7"
     }
   }
+#  tags {
+#    minecraft-server
+#  }
 
   network_interface {
     network = "default"
@@ -32,19 +22,25 @@ resource "google_compute_instance" "default" {
     }
   }
 
-  metadata_startup_script = "echo hi > /test.txt"
+  metadata_startup_script = "${data.template_file.default.rendered}"
+#  user_data = data.template_file.default.rendered
 
 }
 output "public_ip" {
     value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
+output "reder_scrip" {
+    value = data.template_file.default.rendered
+
+}
+
 
 
 data "template_file" "default" {
   template = "${file("start-minecraft-server.tpl")}"
   vars = {
     minecraft-version = "1.17.1"
-    minecraft-download-spigot = "https://download.getbukkit.org/spigot/spigot-${minecraft-version}.jar"
+    minecraft-download-spigot = "https://download.getbukkit.org/spigot/spigot"
     openjdk = "java-latest-openjdk-16.0.1.0.9-3.rolling.el7.x86_64"
     minecraft-core = "/var/opt/minecraft"
     minecraft-words = "words"
--- start-minecraft-server.tpl
+++ start-minecraft-server.tpl
@@ -1,5 +1,9 @@
-#!/bin/bash env
-yum install ${openjdk} tmux
+#!/bin/bash 
+yum install ${openjdk} tmux wget zsh git -y 
 mkdir ${minecraft-core}
 cd ${minecraft-core}
-wget minecraft-download-spigot
+wget ${minecraft-download-spigot}-${minecraft-version}.jar -O ${minecraft-core}/spigot${minecraft-version}.jar
+echo "eula=true" >> ${minecraft-core}/eula.txt
+CHSH=no sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+tmux new-session -d -s Minecraft-Server 'java -jar ${minecraft-core}/spigot${minecraft-version}.jar'
