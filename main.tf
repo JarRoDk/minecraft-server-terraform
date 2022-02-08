aecb5e0b7013f023281858cfc53c5983ffe41a7f
JarRoD <jaroslaw.nyka@tameshi.pl>
2022-02-08 21:22:30 +0100

    FEAT: run terraform fmt on repo

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "google_compute_instance" "default" {
   name         = "minecraft-temporary-server-centos7"
   machine_type = "e2-standard-2"
   zone         = "europe-central2-a"
-  project = "quantum-bonus-325717"
+  project      = "quantum-bonus-325717"
 
 
   boot_disk {
@@ -14,8 +14,8 @@ resource "google_compute_instance" "default" {
       image = "centos-cloud/centos-7"
     }
   }
-  tags = [ "minecraft-server" ]
-  
+  tags = ["minecraft-server"]
+
 
   network_interface {
     network = "default"
@@ -25,50 +25,50 @@ resource "google_compute_instance" "default" {
     }
   }
 
-  metadata_startup_script = "${data.template_file.default.rendered}"
+  metadata_startup_script = data.template_file.default.rendered
 
- service_account {
+  service_account {
     email  = data.google_compute_default_service_account.default.email
     scopes = ["cloud-platform"]
   }
   scheduling {
-    preemptible = true
+    preemptible       = true
     automatic_restart = false
   }
 
-#  metadata = {
-#    shutdown-script = "/usr/bin/sh /opt/minecraft/backup-all.sh"
-#  }
+  #  metadata = {
+  #    shutdown-script = "/usr/bin/sh /opt/minecraft/backup-all.sh"
+  #  }
 }
 
 data "google_secret_manager_secret_version" "key" {
   project = "quantum-bonus-325717"
-  secret   = "new_relic_key"
-  version  = "1"
+  secret  = "new_relic_key"
+  version = "1"
 }
 
-data  "google_secret_manager_secret_version" "apikey" {
+data "google_secret_manager_secret_version" "apikey" {
   project = "quantum-bonus-325717"
-  secret   = "new_relic_api_key"
-  version  = "1"
+  secret  = "new_relic_api_key"
+  version = "1"
 }
 
 
 data "template_file" "default" {
-  template = "${file("start-minecraft-server.tpl")}"
+  template = file("start-minecraft-server.tpl")
   vars = {
     minecraft-version = "1.17.1"
-#    minecraft-download-spigot = "https://download.getbukkit.org/spigot/spigot" need to move it to build script
-    openjdk = "java-latest-openjdk-16.0.1.0.9-3.rolling.el7.x86_64"
+    #    minecraft-download-spigot = "https://download.getbukkit.org/spigot/spigot" need to move it to build script
+    openjdk        = "java-latest-openjdk-16.0.1.0.9-3.rolling.el7.x86_64"
     minecraft-core = "/opt/minecraft"
-    minecraft-bin = "spigot-server"
+    minecraft-bin  = "spigot-server"
     minecraft-maps = "maps"
-    map-prefix = "map"
-#    realm = "2mm"
-    realm = "3magda-jasin-agata"
+    map-prefix     = "map"
+    #    realm = "2mm"
+    realm                       = "3magda-jasin-agata"
     simplybackup-interval-hours = "2"
-    key = data.google_secret_manager_secret_version.key.secret_data 
-    api_key = data.google_secret_manager_secret_version.apikey.secret_data
+    key                         = data.google_secret_manager_secret_version.key.secret_data
+    api_key                     = data.google_secret_manager_secret_version.apikey.secret_data
   }
 }
 
--- output.tf
+++ output.tf
@@ -2,5 +2,5 @@
 #    value = data.template_file.default.rendered
 #}
 output "public_ip" {
-    value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
