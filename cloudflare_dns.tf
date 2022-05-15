bd58fa4b51659e5d695d9685d2bb06b9f6b25e5c
Jaroslaw Nyka <jaroslaw.nyka@luminorgroup.com>
2022-05-15 17:32:08 +0200

    FIX: change hostname to dns_name

--- cloudflare_dns.tf
+++ cloudflare_dns.tf
@@ -1,10 +1,11 @@
 resource "cloudflare_record" "minecraft_dns" {
   zone_id = lookup(data.cloudflare_zones.minecraft_zone.zones[0], "id")
-  name    = var.dns_zone
+  name    = var.dns_name
   value   = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
   type    = "A"
-  proxied = true
+  proxied = false
   allow_overwrite = true
+  ttl = 1
 }
 data "cloudflare_zones" "minecraft_zone" {
   filter {
